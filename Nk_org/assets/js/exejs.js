b65cef39e1bf34b0bb10c583669d7d80cfe5d8b0
Jaybco <jedidahjayb@gmail.com>
2024-11-20 18:51:13 +0530

    Add files via upload

---
+++ Nk_org/assets/js/exejs.js
@@ -0,0 +1,36 @@
+*/
+//var optionET=parseInt(prompt(
+var option=parseInt (prompt(Entrer votre option));
+switch(option){
+case"GL"
+alert("Genie Logiciel");
+break;
+case "SI"
+alert("Systeme Informatique");
+break;
+case "LIAGE"
+alert("Informatique Appliquer a la Gestion des Entreprise");
+break;
+default
+alert("erreur");
+}
+*/
+
+var age=document.getElementByild("age").value;
+if(age==""){
+document.getElementByild("reponse").value="Entrer votre age";
+}else if(age>=18){
+document.getElementByild("reponse").value="majeur";
+}else{
+document.getElementByild("reponse").value="mineur"
+}
+
+function exetranslateday(){
+
+}
+
+
+
+
+
+
